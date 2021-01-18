a720c30d2d8d0b798c8b1141bccf9f6f6e1e4532
Simon <Xu>
2021-01-18 12:18:06 -0800

    Changed HAL_Delay to 3 seconds

--- Src/main.c
+++ Src/main.c
@@ -121,7 +121,7 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    HAL_Delay(2000);
+    HAL_Delay(3000);
     // Causes the LED to toggle back and forth
     HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
     // printf("Hello World");
