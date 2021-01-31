8b600f63ba54bf0d611c22e9077345b1edfee905
Simon <Xu>
2021-01-31 10:16:42 -0800

    classes learning from Martti

--- Src/classtest.cpp
+++ Src/classtest.cpp
@@ -1,5 +1,11 @@
 #include "classtest.h"
-#include "usbd_cdc_if.h"
+#include "usbd_cdc_if.h"                // Needed for CDC_Transmit_FS
 
-uint8_t FatherProperty::propValue[]="asdf\n";
-void FatherProperty::getProperty() {CDC_Transmit_FS(this->propValue,5);}
+// uint8_t FatherProperty::propValue[]="asdf\n";
+void FatherProperty::getProperty() 
+{
+    int len = sizeof(this->propValue);
+    CDC_Transmit_FS(this->propValue,len-1);         // Option 1: can directly print
+    // CDC_Transmit_FS((uint8_t*)this->propValue,len-1);         // Option 2: require a type conversion
+
+}
--- include/classtest.h
+++ include/classtest.h
@@ -1,10 +1,17 @@
 
 #include <stdint.h>
+#include <string.h>
 
 class FatherProperty {
 private:
-    static uint8_t propValue[];        // I need to declare this as static, why?
+    uint8_t propValue[8];        // Option 1: keep it as a uint8_t
+    // char propValue[8];        // Option 2: Use char since it is strings
+    // static means that there is only one copy of propValue, no matter how many times the class is instantiated
 
 public:
+    FatherProperty(){               // Define value in constructor
+        for(int i=0;i<8;i++){propValue[i]=i;};       // Option 1: Need loop
+        // strcpy(propValue,"asdf\n");              // Option 2: use strcpy instead
+    };
     void getProperty();
 };
